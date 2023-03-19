44b7809caf52f92cb6b299693cf227a98558030c
Lung <lung@debian-BULLSEYE-live-builder-AMD64>
2023-03-19 22:01:55 +0100

    1st commit

---
+++ src/pages/PageAbout.vue
@@ -0,0 +1,21 @@
+<template>
+    <q-page>
+        <h4 class="q-mt-none q-md-mb text-weight-bold">About this Twitter clone</h4>
+        <div class="text-body1">
+        <p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt ornare massa eget egestas purus viverra accumsan in nisl. Diam maecenas sed enim ut sem viverra. Cras pulvinar mattis nunc sed blandit libero. Ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Condimentum lacinia quis vel eros donec. Venenatis urna cursus eget nunc scelerisque viverra mauris. Sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra. Sit amet venenatis urna cursus eget nunc. Habitant morbi tristique senectus et netus et malesuada fames ac. Elementum pulvinar etiam non quam. Lacus sed turpis tincidunt id aliquet risus feugiat in ante. Eu lobortis elementum nibh tellus. Massa tincidunt dui ut ornare lectus sit amet. Consectetur adipiscing elit duis tristique sollicitudin nibh. In cursus turpis massa tincidunt. Ante metus dictum at tempor. Natoque penatibus et magnis dis. Vel orci porta non pulvinar neque. Ornare suspendisse sed nisi lacus sed viverra tellus in.
+
+</p>
+</div>
+
+    </q-page>
+  </template>
+  
+  <script>
+  import { defineComponent } from 'vue'
+  
+  export default defineComponent({
+    name: 'PageAbout'
+  })
+  </script>
+  
---
+++ src/pages/PageHome.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-page>
+    <div class="q-py-lg q-px-md">
+    <q-input bottom-slots v-model="newTweetContent" placeholder="What's happening?" counter maxlength="256">
+        <template v-slot:before>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/avatar5.jpg">
+          </q-avatar>
+        </template>
+
+        <template v-slot:append>
+          <q-icon v-if="text !== ''" name="close" @click="text = ''" class="cursor-pointer" />
+          <q-icon name="schedule" />
+        </template>
+
+        <template v-slot:hint>
+          Field hint
+        </template>
+
+        <template v-slot:after>
+          <q-btn round dense flat icon="send" />
+        </template>
+      </q-input>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageHome'
+})
+
+</script>
