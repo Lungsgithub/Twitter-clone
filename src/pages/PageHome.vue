4f8c86c0aba7ff191cab5a0b02c7625c709b0c8e
Lungsgithub <107316499+Lungsgithub@users.noreply.github.com>
2023-03-28 20:12:35 +0200

    Add files via upload

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,35 +1,52 @@
 <template>
   <q-page>
-    <div class="q-py-lg q-px-md">
-    <q-input bottom-slots v-model="newTweetContent" placeholder="What's happening?" counter maxlength="256">
+    <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+      <div class="col">
+
+        <q-input class="newTweet" autogrow bottom-slots v-model="newTweetContent" placeholder="What's happening?" counter maxlength="256">
         <template v-slot:before>
-          <q-avatar>
+          <q-avatar size="xl">
             <img src="https://cdn.quasar.dev/img/avatar5.jpg">
           </q-avatar>
         </template>
-
-        <template v-slot:append>
-          <q-icon v-if="text !== ''" name="close" @click="text = ''" class="cursor-pointer" />
-          <q-icon name="schedule" />
-        </template>
-
-        <template v-slot:hint>
-          Field hint
-        </template>
-
-        <template v-slot:after>
-          <q-btn round dense flat icon="send" />
-        </template>
       </q-input>
+
+      </div>
+
+      <div class="col col-shrink">
+      <q-btn :disable="!newTweetContent"  class="q-mb-lg" unelevated rounded nocaps color="primary" label="tweet" />
+      </div>
     </div>
+
+
+    <q-serparator size="15px" />
+
+
+
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
 
-export default defineComponent({
-  name: 'PageHome'
-})
+export default{
+  name: 'PageHome',
+  data(){
+    return{
+      newTweetContent:""
+    }
+  }
+}
 
 </script>
+
+<!-- I still wanna make this external -->
+
+<style lang="sass">
+.newTweet
+  textarea
+  font-size: 19px
+  line-height: 1.4
+
+  
+</style>
